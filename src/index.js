fa201f311a4ec3cde0579cbbc4b7864c39002008
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2023-05-25 00:52:38 +0600

    done: creating docker image, pushed to dockerhub

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const app = require("express")();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Docker is easy"
+    });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`app listening to http://localhost:${port}`);
+});
